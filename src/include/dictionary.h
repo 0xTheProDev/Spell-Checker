d00138e0a54b4de91f4e4401e499230f487052f7
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-02-15 19:45:36 +0530

    Release 1.0

---
+++ src/include/dictionary.h
@@ -0,0 +1,72 @@
+/*
+ * DICTIONARY.H
+ * 
+ * Definitions of types and prototypes of functions for operations on
+ * BigInteger formatted data
+ *
+ * Written by Progyan Bhattacharya <bprogyan@gmail.com>
+ * GitHub Repository: https://github.com/Progyan1997/Spell-Checker
+ * Copyright (C) 2017, GNU Public License 3.0
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, this permission notice, and the following
+ * disclaimer shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OF OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * NOTE: The file manipulation functions provided by Microsoft seem to
+ * work with either slash (/) or backslash (\) as the directory separator;
+ * (this is consistent with Microsoft's own documentation, on MSDN).
+ *
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+#define NUL '\0'
+#define EOL '\n'
+#define MAX_SIZE 27
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word);
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+#endif // DICTIONARY_H
---
+++ src/main.c
@@ -0,0 +1,172 @@
+/*
+ * MAIN.C
+ * 
+ * Implementation of DICTIONARY.H
+ *
+ * Written by Progyan Bhattacharya <bprogyan@gmail.com>
+ * GitHub Repository: https://github.com/Progyan1997/Spell-Checker
+ * Copyright (C) 2017, GNU Public License 3.0
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, this permission notice, and the following
+ * disclaimer shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OF OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * NOTE: The file manipulation functions provided by Microsoft seem to
+ * work with either slash (/) or backslash (\) as the directory separator;
+ * (this is consistent with Microsoft's own documentation, on MSDN).
+ *
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+
+#include "include/dictionary.h"
+#undef calculate
+#undef getrusage
+
+// default dictionary
+#define DICTIONARY "dictionaries/large"
+
+int main(int argc, char* argv[])
+{
+    // check for correct number of args
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: speller [dictionary] text\n");
+        return 1;
+    }
+
+    // determine dictionary to use
+    char* dictionary = (argc == 3) ? argv[1] : DICTIONARY;
+
+    // load dictionary
+    bool loaded = load(dictionary);
+
+    // abort if dictionary not loaded
+    if (!loaded)
+    {
+        printf("Could not load %s.\n", dictionary);
+        return 1;
+    }
+
+    // try to open text
+    char* text = (argc == 3) ? argv[2] : argv[1];
+    FILE* fp = fopen(text, "r");
+    if (fp == NULL)
+    {
+        printf("Could not open %s.\n", text);
+        unload();
+        return 1;
+    }
+
+    // prepare to report misspellings
+    printf("\nMISSPELLED WORDS\n\n");
+
+    // prepare to spell-check
+    int index = 0, misspellings = 0, words = 0;
+    char word[LENGTH+1];
+
+    // spell-check each word in text
+    for (int c = fgetc(fp); c != EOF; c = fgetc(fp))
+    {
+        // allow only alphabetical characters and apostrophes
+        if (isalpha(c) || (c == '\'' && index > 0))
+        {
+            // append character to word
+            word[index] = c;
+            index++;
+
+            // ignore alphabetical strings too long to be words
+            if (index > LENGTH)
+            {
+                // consume remainder of alphabetical string
+                while ((c = fgetc(fp)) != EOF && isalpha(c));
+
+                // prepare for new word
+                index = 0;
+            }
+        }
+
+        // ignore words with numbers (like MS Word can)
+        else if (isdigit(c))
+        {
+            // consume remainder of alphanumeric string
+            while ((c = fgetc(fp)) != EOF && isalnum(c));
+
+            // prepare for new word
+            index = 0;
+        }
+
+        // we must have found a whole word
+        else if (index > 0)
+        {
+            // terminate current word
+            word[index] = '\0';
+
+            // update counter
+            words++;
+
+            // check word's spelling
+            bool misspelled = !check(word);
+
+            // print word if misspelled
+            if (misspelled)
+            {
+                printf("%s\n", word);
+                misspellings++;
+            }
+
+            // prepare for next word
+            index = 0;
+        }
+    }
+
+    // check whether there was an error
+    if (ferror(fp))
+    {
+        fclose(fp);
+        printf("Error reading %s.\n", text);
+        unload();
+        return 1;
+    }
+
+    // close text
+    fclose(fp);
+
+    // determine dictionary's size
+    unsigned int n = size();
+
+    // unload dictionary
+    bool unloaded = unload();
+
+    // abort if dictionary not unloaded
+    if (!unloaded)
+    {
+        printf("Could not unload %s.\n", dictionary);
+        return 1;
+    }
+
+
+    // report benchmarks
+    printf("\nWORDS MISSPELLED:     %d\n", misspellings);
+    printf("WORDS IN DICTIONARY:  %d\n", n);
+    printf("WORDS IN TEXT:        %d\n", words);
+    
+    return 0;
+}
